c5fc934ca39ba69841bc6018f90321cf96e36d37
Dmitrij Sokolov <darkfalken88@gmail.com>
2025-10-01 17:19:30 +0300

    Project created

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,28 @@
+package ru.netology.web.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.web.data.DataHelper.getAuthInfo;
+
+public class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenCards() {
+        var info = getAuthInfo();
+        var verificationCode = DataHelper.getVerificationCodeFor(info);
+
+        var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
+        var verificationPage = loginPage.validLogin(info);
+        verificationPage.verifyPageVisibility();
+        var dashBoardPage = verificationPage.validVerify(verificationCode);
+
+
+        $("[data-test-id=dashboard]").should(Condition.visible);
+
+    }
+}
